8ac304c18561ecc2c915fc7a71b0d964958dcb4e
DavisHill-SS <davis.hill@smoothstack.com>
2021-08-26 21:58:34 -0500

    added view loantypes

--- src/main/java/com/beardtrust/webapp/loanservice/controllers/LoanTypeController.java
+++ src/main/java/com/beardtrust/webapp/loanservice/controllers/LoanTypeController.java
@@ -4,10 +4,9 @@ import com.beardtrust.webapp.loanservice.entities.LoanTypeEntity;
 import com.beardtrust.webapp.loanservice.services.LoanTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/loantypes/")
--- src/main/java/com/beardtrust/webapp/loanservice/services/LoanTypeService.java
+++ src/main/java/com/beardtrust/webapp/loanservice/services/LoanTypeService.java
@@ -1,7 +1,8 @@
 package com.beardtrust.webapp.loanservice.services;
 
 import com.beardtrust.webapp.loanservice.entities.LoanTypeEntity;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public interface LoanTypeService {
 
